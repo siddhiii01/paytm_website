3f17b2c901d0c7dd1f10bd5eaa08f4f833a339f1
siddhiii01 <siddhiigupta15@gmail.com>
2025-11-18 18:33:53 +0530

    Prisma Setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,25 @@
 import express from "express";
+import type {Request, Response, NextFunction} from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
 const app = express();
+
+app.use(express.json()); //to parse json for req.body, post, patch req 
+
+//Logging Middleware
+app.use((req: Request,res:Response, next: NextFunction) => {
+    console.log("Incoming req: ");
+    console.log(`req.headers: ${JSON.stringify(req.headers)}`)
+    console.log(`req.bpdy: ${JSON.stringify(req.body)}`);
+    console.log(`req.url: ${req.originalUrl}`);
+    next();
+})
+
+app.get('/test', (req: Request, res: Response) => {
+    console.log("Test Route");
+    res.send('Test Route');
+})
 
 app.listen(process.env.PORT, () => {
     console.log("Server is running")
