0b8394c16197ab65b9814e58b458e6742eb2fee5
siddhiii01 <siddhiigupta15@gmail.com>
2025-12-04 23:46:16 +0530

    Cleanuped up Pytm-wallet code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,35 +1,13 @@
 import express from "express";
 import type {Request, Response, NextFunction} from "express";
-import dotenv from "dotenv";
-
 import {prisma, connectDB} from "./db/prisma.js"
-
-import z, { success } from "zod";
-import bcrypt from "bcrypt"
-import e from "cors";
 import { appConfig } from "@config/app.config.js";
-import { authConfig } from "@config/auth.config.js";
-import { successResponse, errorResponse, serverResponse } from "@utils/response.utils.js";
 import { AuthController } from "@controllers/auth.controllers.js";
 import { AuthMiddleware } from "@middlewares/auth.middleware.js";
 import cookieParser from "cookie-parser";
-import {initiateOnRampSchema} from "@validations/onramp.schema.js"
-import { OnRampController } from "@controllers/onramp.controllers.js";
-
-
 
 const app = express();
 app.use(cookieParser());
-
-
-
-
-
-const SALT_ROUNDS: number = 10; //tells how expensive hashing algorith shud be
-
-const privateKey = process.env.JWT_PRIVATE_KEY as string;
-const publicKey = process.env.JWT_PUBLIC_KEY as string;
-
 
 //every incoming requesting that browser is sending is goes through this middleware before acutally hitting our route
 //This looks at header: Content-Type: application/json
@@ -44,113 +22,6 @@ app.use(express.json());
 //     next();
 // });
 
-app.get('/test', (req: Request, res: Response) => {
-  res.send("testing route")
-});
-
-// app.post('/data', (req: Request, res:Response) => {
-//     const {name} = req.body;
-//     const {id} = req.query;
-
-//     res.status(200).json({
-//       data: [req.body, req.query]
-//     })
-// })
-
-// Initialize Prisma with adapter
-// const prisma = new PrismaClient({});
-
-
-
-
-// async function testConnection() {
-//   try {
-//     // Test database connection
-//     await prisma.$connect();
-//     console.log('Prisma connected successfully!');
-//   } catch (error) {
-//     console.error('Prisma connection failed:', error);
-//   } finally {
-//     console.log("Prisma disconnected");
-//     await prisma.$disconnect();
-//   }
-// }
-
-// testConnection();
-
-// app.get('/test', (req: Request, res: Response) => {
-//     console.log("Test Route");
-//     res.send('Test Route');
-// })
-
-// const token = jwt.sign({foo: 'bar'}, privateKey)
-// console.log(`JWT TOKEN RECEIVED FROM SERVER: ${token}`);
-
-// const decode = jwt.decode(token) as JwtPayload | null;
-// console.log(`DECODE JWT: ${decode}`);
-
-// try {
-//   const verified = jwt.verify(token, privateKey) as JwtPayload;
-//   console.log("VERIFYING JWT TOKEN:", verified);
-// } catch (error) {
-//   console.error("JWT verification failed:", error);
-// }
-
-// const users: any= []
-
-// function hashedFunction(password: any){
-//   return "_hashed" + password
-// }
-
-// app.post('/api/signup', (req, res) => {
-
-//   const { name, password } = req.body;
-
-//   // 1. INPUT VALIDATION
-//   if (!name || !password) {
-//     return res.status(400).json({
-//       success: false,
-//       message: "Missing fields",
-//     });
-//   }
-
-//   if (password.length < 6) {
-//     return res.status(400).json({
-//       success: false,
-//       message: "Password length is too short",
-//     });
-//   }
-
-//   // 2. CHECK EXISTING USER
-//   const existingUser = users.find(u => u.name === name);
-  
-//   if (existingUser) {
-//     return res.status(409).json({
-//       success: false,
-//       message: "User already exists",
-//     });
-//   }
-
-//   // 3. CREATE USER
-//   const hasedpassword = hashedFunction(password)
-//   const newUser = { name, hasedpassword };
-//   users.push(newUser);
-
-//   // 4. SEND FINAL RESPONSE
-//   return res.status(201).json({
-//     success: true,
-//     message: "New user created",
-//     data: newUser
-//   });
-// });
-
-
-// app.post('/api/login', (req,res) => {
-//   //check if the users exist in a database or not -> if he doesn't redirect to him on /api/signup page
-//   //if the user exist in databse fetch the info and check the crendentials maybe
-//   //generate a token 
-// })
-
 
 async function testConnection(){
   const isConnected = await connectDB();
@@ -159,121 +30,27 @@ async function testConnection(){
     process.exit(1) //stop the whole server is db is not connected
   }
 }
-
 testConnection();
 
-
-async function main(){
-  //create a new user
-  const user = await prisma.user.create({
-    data: {
-      name: "Bob",
-      email: "Bob@prisma.io",
-      password: "Bob$$$"
-    }
-  });
-
-
-  console.log("User created");
-
-  const allUser = await prisma.user.findMany({});
-  console.log("All users: ", JSON.stringify(allUser, null, 2))
-}
-
-//main()
-
-  // app.post('/zod-testing', async (req: Request, res: Response) => {
-  //   try{
-  //     //Validates request body
-  //     const userData = SignupSchema.parse(req.body);
-
-  //     //userData is now 100% safe
-  //     const user = await prisma.user.create({data: userData});
-
-  //     res.status(201).json({
-  //       success: true,
-  //       user
-  //     });
-
-  //   } catch(error){
-  //       console.error(error)
-  //   }
-  // });
-
 //signup route
-app.post('/signup',  AuthController.register, async (req, res) => {
-   
-  
-});
-
+app.post('/signup', AuthController.register);
 
 //login route
-app.post('/login',AuthController.login, async (req, res) => {
-  
-  
-});
+app.post('/login',AuthController.login);
 
 //refresh token
 app.post('/refreshToken', AuthMiddleware.authenticateUser, AuthController.refreshToken, (req,res) => {
   res.json({message:'refresh token page '})
-})
+});
 
 //logout page
-
 app.get('/logout',AuthMiddleware.authenticateUser, AuthController.logout, (req, res) => {
   console.log((req as any).user?.userId)
-})
-
-// console.log("App Config: ", appConfig)
-// console.log("Auth Config: ", authConfig)
-
-//testing
-app.get("/success", (req, res) => {
-  successResponse(res)//default parameter
-})
-app.get("/error", (req, res) => {
-  errorResponse(res, 'Something went wrong with your request')//default parameter
-})
-app.get("/server", (req, res) => {
-  serverResponse(res, )//default parameter
-})
-
+});
 
 app.get("/auth", AuthMiddleware.authenticateUser, (req, res) => {
   console.log("req.userId", (req as any).userId)
 });
-
-
-
-
-//endpoint for user to start adding money
-app.post("/onramp",AuthMiddleware.authenticateUser, OnRampController.initiateOnRampTx, (req: Request, res: Response) => {
- 
-  // const result = initiateOnRampSchema.safeParse(req.body);
-  // if(!result.success){
-  //   return res.status(400).json({
-  //     error: result.error.flatten()
-  //   })
-  // }
-
-  // const data = result.data; // validated & typed
-
-  // res.json({
-  //   message: "Onramp request received",
-  //   data
-  // });
-  // //this is a route that accepts amt and payment details
-  // const {amount, provider, userId, } = (req ).body;
-
-  // res.json({
-  //   message: "Onramp request received",
-  //   data: { amount, provider, userId }
-  // });
-
-  // console.log(amount, provider, userId, );
-  
-})
-
 
 app.listen(appConfig.port, ()=>{
   console.log("Server is running")
