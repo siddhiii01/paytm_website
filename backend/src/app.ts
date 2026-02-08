0bc084038c089e0e6db697f748474ba6d237dbd1
siddhiii01 <siddhiigupta15@gmail.com>
2026-02-09 00:14:09 +0530

    updated

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -26,7 +26,7 @@ const app = express();
 
 app.use(errorHandler)
 app.use(cors({
-  origin: "http://localhost:5173",  
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",  
   credentials: true,   //allow browser to send cookies when  making req from this origin -> cookies allowed
 }));
 
